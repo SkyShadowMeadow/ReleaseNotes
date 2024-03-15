daefe3492adb5b9295c7daf1ed576dc65d46fcd3
Catherine <catherine.semiletova@gmail.com>
2024-03-15 16:30:55 +0100

    Add AI models to use in for requests

---
+++ Models/AIModel.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Models
+{
+    public class AIModel
+    {
+        public string openAIKey {get;}
+        public string openAIUrl {get;}
+        public string modelName {get;}
+
+        public AIModel(string openAIKey,string openAIURL, string modelName)
+        {
+            this.openAIKey = openAIKey;
+            this.openAIUrl = openAIURL;
+            this.modelName = modelName;
+        }
+    }
+}
---
+++ Models/AiRequestModel.cs
@@ -0,0 +1,8 @@
+namespace Models
+{
+    public class AIRequestModel
+    {
+        public string Role { get; set; }
+        public string Content { get; set; }
+    }
+}
